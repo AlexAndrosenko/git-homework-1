f2420919f4dfcc5ce6e233451c9d7d09bbe61c75
AlexAndrosenko <tyraelne@gmail.com>
2026-03-25 01:47:36 +0200

    function homework1

---
+++ funcBasics/task1.js
@@ -0,0 +1,13 @@
+//declaration
+function square(width, height) {
+    return width * height;
+}
+console.log(square(3,5));
+//expression
+const squareExp = function(width, height) {
+    return width * height;
+}
+console.log(squareExp(2,6));
+//arrow
+const squareArrow = (width, height) => width * height;
+console.log(squareArrow(4,7));
