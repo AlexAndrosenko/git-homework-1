061a11af3a7e67a5705e0e1e1412a93b43afa69f
AlexAndrosenko <tyraelne@gmail.com>
2026-03-28 20:01:42 +0200

    Just home work 7 added

---
+++ funcAdvanced/task1.js
@@ -0,0 +1,17 @@
+function handleNum (a, b, c) { 
+if (a % 2 === 0) {
+    return b();
+}  else {
+    return c();
+}
+}
+
+function evenNum() {
+    return console.log("number is even");
+}
+
+function oddNum() {
+    return console.log("number is odd");
+}
+
+handleNum(213, evenNum, oddNum);
